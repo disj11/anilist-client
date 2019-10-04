3a11cb244f8c7c0537c8a274d44548b92c334989
tykim <disj11@naver.com>
2019-10-04 18:27:34 +0900

    Updates

---
+++ precache-manifest.d342f88176b5d11ff091157e882343a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611cdff512ea999dce354b3ef74ff305",
+    "url": "/anilist-client/index.html"
+  },
+  {
+    "revision": "fce83c899aa03f2eda5d",
+    "url": "/anilist-client/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "bd240e49ebb65d723209",
+    "url": "/anilist-client/static/css/main.2e9e664a.chunk.css"
+  },
+  {
+    "revision": "fce83c899aa03f2eda5d",
+    "url": "/anilist-client/static/js/2.9dd0e3a9.chunk.js"
+  },
+  {
+    "revision": "bd240e49ebb65d723209",
+    "url": "/anilist-client/static/js/main.7226cce8.chunk.js"
+  },
+  {
+    "revision": "cc7a433e5f57af12a950",
+    "url": "/anilist-client/static/js/runtime-main.f7e5102d.js"
+  }
+]);
