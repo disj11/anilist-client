92578c69604e4bb20d97232de2942be22e5db791
disj11 <disj11@naver.com>
2020-06-15 22:44:22 +0900

    css 사용 못 하도록 변경

--- src/components/atoms/transition/AppearTransition.tsx
+++ src/components/atoms/transition/AppearTransition.tsx
@@ -1,18 +1,43 @@
-import {Transition} from "react-transition-group";
 import React, {FunctionComponent} from "react";
 import {Box} from "@material-ui/core";
-import "./AppearTransition.css";
+import {makeStyles} from "@material-ui/core/styles";
+import {Transition} from "react-transition-group";
+
+const useStyles = makeStyles(() => ({
+    "@keyframes appear": {
+        "0%": {
+            left: 100,
+            opacity: 0,
+        },
+        "100%": {
+            left: 0,
+            opacity: 1,
+        }
+    },
+    root: {
+        '& > .transition-box': {
+            position: "relative",
+        },
+        '& > .transition-box-entering': {
+            animationName: "$appear",
+            animationDuration: "0.3s"
+        },
+    },
+}));
 
 interface Props {
     transitionIn: boolean;
 }
 
 const AppearTransition: FunctionComponent<Props> = ({children, transitionIn}) => {
+    const classes = useStyles();
     return (
         <Transition timeout={3000} in={transitionIn} appear>
             {status => (
-                <Box className={`transition-box transition-box-${status}`}>
-                    {children}
+                <Box className={classes.root}>
+                    <Box className={`transition-box transition-box-${status}`}>
+                        {children}
+                    </Box>
                 </Box>
             )}
         </Transition>
